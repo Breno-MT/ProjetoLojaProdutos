cb0788133a92ec9daadeb81ff9f4df4fcf3167e8
Breno-MT <hentgenp@gmail.com>
2022-04-05 18:21:51 -0300

    Adicionado Funções
    Adicionei a função RemoveItem que irá tirar um item da lista, criei também a função addItem para adicionar um item, fiz a function updateScreen para criar uma nova ul com li dentro e junto coloquei um botao X para excluir um item que o usuario desejar, coloquei localStorage para salvar no localstorage. Transformei a lista em uma JSON.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,90 @@
 var inputText = document.getElementById('produto');
 var btnDeleteAll = document.getElementById('btn-deletarTudo');
 var btnSubmit = document.getElementById('btn-submit');
+var ul = document.getElementById('lista');
 
 
 
-var listaProdutos = [];
+var lista = [];
 
+var listaJSON = localStorage.getItem('lista')
+if (listaJSON) {
+    lista = JSON.parse(listaJSON);
+    updateScreen();
+}
 
+function saveStorage() {
+    var listaJSON = JSON.stringify(lista);
+    localStorage.setItem('lista', listaJSON);
+}
 
-
-
-btnSubmit.addEventListener('click', () => {
-    var ul = document.getElementById('lista');
-    var li = document.createElement('li');
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-
+function removeItem(id) {
+    var novaLista = [];
     
-    li.appendChild(checkbox);
-    li.appendChild(document.createTextNode("bbb"));
-    ul.appendChild(li);
-})
+    lista.forEach(function (item) {
+        if (item.id !== id) {
+            novaLista.push(item)
+        }
+    })
+    
+    lista = novaLista;
+    updateScreen();
+}
+
+function removeAll() {
+   
+}
+
+function updateScreen() {
+    ul.innerHTML = '';
+    
+    lista.forEach(function (item) {
+        var btnX = document.createElement('button');
+        btnX.innerHTML = 'x';
+        btnX.onclick = function () {
+            removeItem(item.id);
+        }
+        
+        var li = document.createElement('li');
+        
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        
+        
+        li.id = `i${item.id}`;
+        li.innerHTML = item.name;
+        
+        ul.appendChild(checkbox);
+        li.appendChild(btnX);
+        ul.appendChild(li);
+    })
+}
+
+
+
+
+
+
+
+function addItem() {
+    if(inputText.value) {
+        lista.push({
+            id: Date.now(),
+            name: inputText.value,
+            price: null,
+        });
+
+        inputText.value = '';
+
+        updateScreen();
+        saveStorage();
+
+    } else {
+        alert("Insira o nome do produto!")
+    }
+}
+
+
+
+btnSubmit.addEventListener('click', addItem);
+btnDeleteAll.addEventListener('click', removeAll);
